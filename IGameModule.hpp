6faf7f50cc3a36dd3f15e31bd922359dadb37d77
Alexandre jublot <alexjublot@gmail.com>
2022-03-22 20:08:48 +0100

    [fix] modified IGameModule method after meeting

--- IGameModule.hpp
+++ IGameModule.hpp
@@ -2,9 +2,18 @@ namespace arcade
 {
     class IGameModule
     {
+
+        /** @returns if this library is the menu */
+        bool isMenu();
+
+        /** @returns the path of the coreboard file */
+        std::string scoreboardPath();
+
         /**
-         * @returns (1) if next game, (-1) if previous game, (0) if end, (84) if error
+         * @returns int (status code): (1) if next game, (-1) if previous game, (0) if end, (84) if error
+         *          string : path to actual .so grpahical lib
          */
-        int run();
+        std::pair<int, std::string> run(std::string pathToGraphLib);
+
     };
 }
